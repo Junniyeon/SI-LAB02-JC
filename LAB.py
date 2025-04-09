7ea319a6b27676af517212e9426eb53b3a7d50ec
junior cachique <janoscachique4@gmail.com>
2025-04-08 19:45:05 -0500

    cambio en lab py

--- LAB.py
+++ LAB.py
@@ -1,8 +1,13 @@
 import numpy as np
 import cv2
-img =cv2.imread('A.jpg')
-cv2.imshow('imagen',img)
-cv2.walKey(0)
+
+img = cv2.imread(r'C:\\Users\\DOCENTE\\Documents\\SI LAB  02\\imagen\\A.jpeg')
+
+
+cv2.imshow('imagen', img)
+
+
+cv2.waitKey(0)
+
+
 cv2.destroyAllWindows()
-
-
