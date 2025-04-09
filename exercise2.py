42de855bbe16f4a9bdd6693bd13cedf36b910d4b
junior cachique <janoscachique4@gmail.com>
2025-04-08 19:57:19 -0500

    cambio 2

--- exercise2.py
+++ exercise2.py
@@ -1,13 +1,15 @@
 import numpy as np
 import cv2
 
-img = cv2.imread(r'C:\\Users\\DOCENTE\\Documents\\SI LAB  02\\imagen\\B.png')
+img_path = r'C:\Users\DOCENTE\Documents\SI LAB  02\imagen\B.png'
+img = cv2.imread(img_path)
 
+width = int(img.shape[1] * 0.3)
+height = int(img.shape[0] * 0.3)
+dim = (width, height)
 
-cv2.imshow('imagen', img)
+resized_img = cv2.resize(img, dim)
 
-
+cv2.imshow('imagen', resized_img)
 cv2.waitKey(0)
-
-
 cv2.destroyAllWindows()
