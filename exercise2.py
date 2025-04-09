1682608ab69ce0ccf4dfb6e6b5f8ccfecaf72c37
junior cachique <janoscachique4@gmail.com>
2025-04-08 19:51:23 -0500

    ejercicio2

---
+++ exercise2.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+img = cv2.imread(r'C:\\Users\\DOCENTE\\Documents\\SI LAB  02\\imagen\\B.png')
+
+
+cv2.imshow('imagen', img)
+
+
+cv2.waitKey(0)
+
+
+cv2.destroyAllWindows()
